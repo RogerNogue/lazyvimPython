0c27b9530857edaa2f978d8b0fb33eb6aae83ec3
RogerNogue <rogernogueb@gmail.com>
2025-06-19 11:13:03 +0200

    refactor: now Fizz cases are calculated using %3

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,12 +1,10 @@
 def FizzBuzzOf(number):
-    if number == 3:
+    if number == 15:
+        return "FizzBuzz"
+    if number % 3 == 0:
         return "Fizz"
     if number == 5:
         return "Buzz"
-    if number == 15:
-        return "FizzBuzz"
-    if number == 6:
-        return "Fizz"
     return number
 
 
