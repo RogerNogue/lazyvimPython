5f1ea32734984061bfd90b5f64a636968050f987
RogerNogue <rogernogueb@gmail.com>
2025-06-19 11:14:09 +0200

    refactor: now Buzz cases are calculated with %5

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -3,7 +3,7 @@ def FizzBuzzOf(number):
         return "FizzBuzz"
     if number % 3 == 0:
         return "Fizz"
-    if number == 5:
+    if number % 5 == 0:
         return "Buzz"
     return number
 
